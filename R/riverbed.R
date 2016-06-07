a6d9ba9f0f3bcc878b39b4ead2952510c978642f
Pascal Kieslich <kieslich@psychologie.uni-mannheim.de>
2016-06-08 00:17:23 +0200

    Extend riverbed functionality and documentation Allow for faceting, remove label arguments, extend function description and examples, add reference, change default scale for fill

--- R/riverbed.R
+++ R/riverbed.R
@@ -1,13 +1,15 @@
 #' Plot density of mouse positions across time steps.
 #' 
-#' \code{mt_plot_riverbed} creates a plot showing the relative frequency of
-#' x-values per time step.
+#' \code{mt_plot_riverbed} creates a plot showing the distribution of one
+#' trajectory variable (e.g., the x-positions or velocity) per time step.
 #' 
-#' This plot indicates where the majority of trajectories lie, and the degree to
-#' which trajectories deviate from the average trajectory. In most data sets,
-#' this plot will result in a horizontal line of high density indicating that
-#' participants took a certain amount of time before deviating from their
-#' starting point.
+#' This function plots the relative frequency of the values of a trajectory 
+#' variable separately for a series of time steps. This type of plot has been
+#' used in previous research to visualize the distribution of x-positions per
+#' time step (e.g., Scherbaum et al., 2010).
+#' 
+#' \code{mt_plot_riverbed} usually is applied to time-normalized trajectory data
+#' as all trajectories are required to have the same number of values.
 #' 
 #' @param data mousetrap data object containing the data to be plotted.
 #' @param use character string specifying the set of trajectories to use in the
@@ -20,12 +22,25 @@
 #'   from the data provided.
 #' @param y_bins number of bins to distribute along the y axis (defaults to
 #'   250).
-#' @param x_label label placed on the output's x axis (defaults to 'Time step').
-#' @param y_label label placed on the output's y axis (defaults to 'X 
-#'   coordinate', as appropriate given the default for \code{y}).
+#' @param facet_row an optional character string specifying a variable in 
+#'   \code{data[[facet_data]]} that should be used for (row-wise) faceting. If 
+#'   specified, separate riverbed plots for each level of the variable will be 
+#'   created.
+#' @param facet_col an optional character string specifying a variable in 
+#'   \code{data[[facet_data]]} that should be used for (column-wise) faceting.
+#'   If specified, separate riverbed plots for each level of the variable will
+#'   be created.
+#' @param facet_data a character string specifying where the (optional) data
+#'   containing the faceting variables can be found.
+#'   
+#' @references Scherbaum, S., Dshemuchadse, M., Fischer, R., & Goschke, T.
+#'   (2010). How decisions evolve: The temporal dynamics of action selection.
+#'   \emph{Cognition, 115}(3), 407-416.
 #' 
 #' @seealso
 #' \link{mt_plot} for plotting trajectory data.
+#' 
+#' \link{mt_time_normalize} for time-normalizing trajectories.
 #' 
 #' @examples
 #' # Load ggplot2
@@ -37,37 +52,33 @@
 #' # Create riverbed plot for all trials
 #' mt_plot_riverbed(mt_example)
 #' 
+#' # Create riverbed plot with custom x and y axis labels
+#' mt_plot_riverbed(mt_example)+
+#'   xlab("Time step")+ylab("X coordinate")
 #' 
-#' # Create separate plots for typical and atypical trials:
+#' # Create separate plots for typical and atypical trials
+#' mt_plot_riverbed(mt_example, facet_col="Condition")
 #' 
-#' # assess range for x-positions across both conditions
-#' xpos_range <- range(mt_example$tn_trajectories[,"xpos",])
-#' 
-#' # create subsets of trials
-#' mt_example_atypical <- mt_subset(mt_example,Condition=="Atypical")
-#' mt_example_typical <- mt_subset(mt_example,Condition=="Typical")
-#' 
-#' # create separate riverbed plots
-#' mt_plot_riverbed(mt_example_atypical,
-#'   use="tn_trajectories", y_range=xpos_range)+
-#'   ggtitle("Atypical condition")
-#' 
-#' mt_plot_riverbed(mt_example_typical,
-#'   use="tn_trajectories", y_range=xpos_range)+
-#'   ggtitle("Typical condition")
+#' # Note that it is also possible to replace the
+#' # default scale for fill with a custom scale
+#' \dontrun{
+#' mt_plot_riverbed(mt_example, facet_col="Condition")+
+#'   scale_fill_gradientn(colours=grDevices::heat.colors(9),
+#'     name="Frequency", trans="log", labels=scales::percent)
+#' }
 #' 
 #' @export
 mt_plot_riverbed <- function(data, use='tn_trajectories', 
-                             y='xpos', y_range=NULL, y_bins=250, 
-                             x_label='Time step', y_label='X coordinate') {
+                             y='xpos', y_range=NULL, y_bins=250,
+                             facet_row=NULL, facet_col=NULL, facet_data='data') {
   
   # Extract data from mousetrap data object
-  data <- extract_data(data=data, use=use)
+  trajectories <- extract_data(data=data, use=use)
   
   # Calculate range of values on y axis,
   # if not specified explicitly
   if (is.null(y_range)) {
-    y_range <- range(data[,y,])
+    y_range <- range(trajectories[,y,])
   }
   
   # Compute breaks based on the number of bins required
@@ -77,46 +88,92 @@ mt_plot_riverbed <- function(data, use='tn_trajectories',
   y_bins <- y_breaks[1:(length(y_breaks) - 1)] + diff(y_breaks) / 2
   
   # Extract number of steps on x axis
-  steps <- dim(data)[3]
+  steps <- dim(trajectories)[3]
+  
+  
+  # Prepare optional facet variables
+  if (!is.null(facet_row)){
+    facet_row_values <- data[[facet_data]][,facet_row]
+    names(facet_row_values) <- data[[facet_data]][,mt_id]
+    facet_row_values <- facet_row_values[dimnames(trajectories)[[1]]]
+  } else {
+    facet_row_values <- rep('constant',each=dim(trajectories)[1])
+    names(facet_row_values) <- dimnames(trajectories)[[1]]
+  }
+  
+  if (!is.null(facet_col)){
+    facet_col_values <- data[[facet_data]][,facet_col]
+    names(facet_col_values) <- data[[facet_data]][,mt_id]
+    facet_col_values <- facet_col_values[dimnames(trajectories)[[1]]]
+  } else {
+    facet_col_values <- rep('constant',each=dim(trajectories)[1])
+    names(facet_col_values) <- dimnames(trajectories)[[1]]
+  }
+  
+  
   
   # Create empty data.frame for plot data
   riverbed <- data.frame()
   
-  # Iterate across x-axis steps
-  for (step in 1:steps) {
-    # Extract data for current step
-    step_data <- data[,,step]
+  
+  # Iterate across facet_row levels
+  for (facet_row_value in unique(facet_row_values)){
     
-    # Calculate histogram based on this data
-    step_hist <- graphics::hist(
-      step_data[,y], 
-      breaks=y_breaks,
-      plot=FALSE
-    )
-    
-    # Append densities and corresponding indices to plot data
-    riverbed <- rbind(
-      riverbed,
-      data.frame(
-        value_x=step,
-        value_y=y_bins,
-        frequency=step_hist$density * diff(y_breaks),
-        alpha=step_hist$density > 0
-      )
-    )
+    # Iterate across facet_col levels
+    for (facet_col_value in unique(facet_col_values)){
+      
+      # Iterate across x-axis steps
+      for (step in 1:steps) {
+        
+        # Extract data for current step
+        step_data <- trajectories[facet_row_value==facet_row_values & facet_col_values==facet_col_value,,step]
+        
+        # Calculate histogram based on this data
+        step_hist <- graphics::hist(
+          step_data[,y], 
+          breaks=y_breaks,
+          plot=FALSE
+        )
+        
+        # Append densities and corresponding indices to plot data
+        riverbed <- rbind(
+          riverbed,
+          data.frame(
+            value_x=step,
+            value_y=y_bins,
+            frequency=step_hist$density * diff(y_breaks),
+            alpha=step_hist$density > 0,
+            facet_row=facet_row_value,
+            facet_col=facet_col_value
+          )
+        )
+      }
+    }
   }
+  
   
   # Create plot output
   output <- ggplot2::ggplot(ggplot2::aes_string(x='value_x', y='value_y', fill='frequency', alpha='alpha'), data=riverbed) +
     ggplot2::geom_raster() +
-    ggplot2::scale_fill_gradient(name='Frequency', trans='log', labels=scales::percent) +
+    ggplot2::scale_fill_gradientn(colours = rev(RColorBrewer::brewer.pal(9,"YlOrRd")),
+                                  name='Frequency',trans='log', labels=scales::percent)+
     ggplot2::scale_alpha_manual(values=c(0, 1), guide='none') +
-    ggplot2::xlab(x_label) + ggplot2::ylab(y_label) +
+    ggplot2::xlab('Steps') + ggplot2::ylab(y) +
     ggplot2::theme(
       panel.background = ggplot2::element_rect(fill='black'),
       panel.grid.major = ggplot2::element_line(colour='gray30'),
       panel.grid.minor = ggplot2::element_line(colour='gray10')
     )
   
-  return(output)
+  # Return output with / without facets depending on the specified variables
+  if(is.null(facet_row) & is.null(facet_col)){
+    return(output)
+  } else {
+    facet_row <- ifelse(is.null(facet_row),'.','facet_row')
+    facet_col <- ifelse(is.null(facet_col),'.','facet_col')
+    facet_formula <- as.formula(paste(facet_row,facet_col,sep='~'))
+    return(output+ggplot2::facet_grid(facet_formula))
+  }
+  
+  
 }
