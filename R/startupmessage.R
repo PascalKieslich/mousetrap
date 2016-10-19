3dd319011d1f1de9d05e19c0e47b1f03358bca7d
Pascal Kieslich <kieslich@psychologie.uni-mannheim.de>
2016-10-19 12:27:51 +0200

    Add startup message for mousetrap

---
+++ R/startupmessage.R
@@ -0,0 +1,6 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to mousetrap 2.0.0!")
+  packageStartupMessage("If you have used previous versions of mousetrap,\nplease note that some function and argument names have changed.")
+  packageStartupMessage("Your old code should still work, but you might get deprecation warnings at some points.")
+  packageStartupMessage("All changes are summarized in the NEWS file.")
+}
