ab833f60f79ec7b5ebe793da21ca9961291060a3
Pascal J. Kieslich <kieslich@psychologie.uni-mannheim.de>
2017-02-09 22:15:21 +0100

    Explicitly specify grDevices library

--- R/utils.R
+++ R/utils.R
@@ -295,20 +295,20 @@ colormixer = function(col1,col2,weight,format = 'rgb'){
   
   if(is.character(col1)){
     if(length(col1) == 1){
-      col1 = col2rgb(col1)
+      col1 = grDevices::col2rgb(col1)
       col1 = matrix(c(col1),ncol=3,nrow=nrows,byrow=T)
     } else {
-      col1 = t(sapply(col1,col2rgb))
+      col1 = t(sapply(col1,grDevices::col2rgb))
     }
   } else{
     col1 = matrix(c(col1),ncol=3,nrow=nrows,byrow=F)
   }
   if(is.character(col2)){
     if(length(col2) == 1){
-      col2 = col2rgb(col2)
+      col2 = grDevices::col2rgb(col2)
       col2 = matrix(c(col2),ncol=3,nrow=nrows,byrow=T)
     } else {
-      col2 = t(sapply(col2,col2rgb))
+      col2 = t(sapply(col2,grDevices::col2rgb))
     }
   } else{
     col2 = matrix(c(col2),ncol=3,nrow=nrows,byrow=F)
@@ -318,7 +318,7 @@ colormixer = function(col1,col2,weight,format = 'rgb'){
   col = col1 * (1-weight) + col2 * weight
   
   if(format == 'rgb') return(col)
-  if(format == 'hex') return(rgb(data.frame(col),maxColorValue = 255))
+  if(format == 'hex') return(grDevices::rgb(data.frame(col),maxColorValue = 255))
   if(!format %in% c('rgb','hex')) stop('Choose either "rgb" or "hex" as format')
   
 }
