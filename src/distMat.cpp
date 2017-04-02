d0a4df10ecdd5935e434cd5cd0e5ff0888d348f6
dwulff <dirk.wulff@gmail.com>
2017-04-02 20:34:06 +0200

    nothing really

--- src/distMat.cpp
+++ src/distMat.cpp
@@ -43,50 +43,52 @@ NumericMatrix distMat(NumericMatrix x,
   return dist;
   }
 
+
+
 // [[Rcpp::export]]
-NumericMatrix distMat(NumericMatrix x,
-                      NumericMatrix y,
-                      int power = 2) {
+NumericMatrix distMatV(NumericMatrix x,
+                       NumericMatrix y,                       
+                       int power = 2) {
   int ni = x.ncol(), nt = x.nrow();
   double xd,yd,d = 0;
   NumericMatrix dist(nt,nt);
-  if(nt > ni){
-    for(int r = 0; r < nt; r++) {     // loop rows
-      for(int c = r; c < nt; c++) {   // loop cols (only half due to symmetry)
-        d = 0;                        //dummy value of final matrix entry
-        if(power == 1){
-          for(int i = 0; i < ni; i++) { // loop 1:n
-            xd = std::abs(x(r,i) - x(c,i));
-            yd = std::abs(y(r,i) - y(c,i));
-            d += xd + yd;
-          }
-        } else if (power == 2){
-          for(int i = 0; i < ni; i++) { // loop 1:n
-            xd = x(r,i) - x(c,i);
-            yd = y(r,i) - y(c,i);
-            d += sqrt( xd*xd  + yd*yd );    
-          }
-        } else {
-          for(int i = 0; i < ni; i++) { // loop 1:n
-            xd = std::abs(x(r,i) - x(c,i));
-            yd = std::abs(y(r,i) - y(c,i));
-            for(int j = 1; j < power; j++){
-              xd *= xd;
-              yd *= yd;
-            }
-            d += exp( log(xd + yd) / double(power));
-          }
+  for(int r = 0; r < nt; r++) {     // loop rows
+    for(int c = r; c < nt; c++) {   // loop cols (only half due to symmetry)
+      d = 0;                        //dummy value of final matrix entry
+      if(power == 1){
+        for(int i = 0; i < ni; i++) { // loop 1:n
+          xd = std::abs(x(r,i) - x(c,i));
+          yd = std::abs(y(r,i) - y(c,i));
+          d += xd + yd;
         }
-        dist(r,c) = d;   // fill distance matrix
-        dist(c,r) = d;   // fill distance matrix
-      }  
-    }
-  } else {
-    
+      } else if (power == 2){
+        for(int i = 0; i < ni; i++) { // loop 1:n
+          xd = x(r,i) - x(c,i);
+          yd = y(r,i) - y(c,i);
+          d += xd*xd  + yd*yd;    
+        }
+      } else {
+        for(int i = 0; i < ni; i++) { // loop 1:n
+          xd = std::abs(x(r,i) - x(c,i));
+          yd = std::abs(y(r,i) - y(c,i));
+          for(int j = 1; j < power; j++){
+            xd *= xd;
+            yd *= yd;
+          }
+          d += xd + yd;
+        }
+      }
+	  if(power == 2){
+        d = sqrt(d);
+        } else if(power > 2){
+        d = exp( log(d) / double(power));
+        }
+      dist(r,c) = d;   // fill distance matrix
+      dist(c,r) = d;   // fill distance matrix
+    }  
   }
   return dist;
 }
-
 
 // [[Rcpp::export]]
 NumericMatrix distMat3d(NumericMatrix x,
