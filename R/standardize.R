a519462d42f151fd47ca5f75f3b93d6eda61370b
dwulff <dirk.wulff@gmail.com>
2017-04-02 19:48:22 +0200

    new, fixed scale trajectories

---
+++ R/standardize.R
@@ -0,0 +1,344 @@
+#' Scale and center variables within the levels of another variable.
+#'
+#' \code{scale_within} centers and/or scales variables in a data.frame (using 
+#' \link{scale}) depending on the levels of one or more other variables. By 
+#' default, variables are standardized (i.e., centered and scaled). A typical 
+#' application is the within-subject standardization of variables in a repeated 
+#' measures design.
+#'
+#' @param data a \link{data.frame}.
+#' @param variables a character string (or vector) specifying one or more
+#'   variables that \link{scale} is applied to. If unspecified,
+#'   \code{scale_within} will be applied to all variables in data.
+#' @param within a character string specifying the name of one or more variables
+#'   in \code{data}. For each of the levels of this variable (or for each
+#'   combination of levels, if more than one variable is specified),
+#'   \link{scale} is applied separately. Alternatively, a vector directly
+#'   containing the level values.
+#' @param prefix a character string that is inserted before each scaled
+#'   variable. By default (empty string) the original variables are replaced.
+#' @param center argument passed on to \link{scale}.
+#' @param scale argument passed on to \link{scale}.
+#'
+#' @return The original data.frame including the centered and / or scaled
+#'   variables.
+#'
+#' @seealso
+#' \link{scale} for the R base scale function.
+#'
+#' \link{mt_standardize} for standardizing measures in a mousetrap data object.
+#'
+#' @examples
+#' ChickWeight_scaled <-  scale_within(
+#'  ChickWeight, variables="weight",
+#'  within="Chick", prefix="z_")
+#'  
+#' @author
+#' Pascal J. Kieslich (\email{kieslich@@psychologie.uni-mannheim.de})
+#' 
+#' Felix Henninger
+#' 
+#' @export
+scale_within <-function(data,
+  variables=NULL, within=NULL,
+  prefix="", center=TRUE, scale=TRUE) {
+
+  # If no variables are specified, operate on all columns
+  if (is.null(variables)) {
+    variables <- colnames(data)
+  }
+
+  # If the within parameter contains less values
+  # than the number of rows of the data.frame,
+  # interpret this as the column names of the grouping
+  # variables. Otherwise, interpret a vector of values
+  # as a grouping factor.
+  if (!is.null(within)) {
+    if (length(within) < nrow(data)) {
+
+      # Extract within variable values
+      within_values <- data[, within, drop=FALSE]
+
+      # Combine within variable values to one
+      # vector coding each combination of values
+      # with a unique integer
+      within_values <- as.numeric(factor(
+        apply(within_values, 1, paste, collapse=";")
+      ))
+
+      variables <- variables[!variables %in% within]
+    } else {
+      within_values <- within
+    }
+  }
+
+  # Standardize each of the variables individually
+  for (var in variables) {
+    # Compute the label for the new,
+    # standardized variable
+    label <- paste(prefix, var, sep="")
+
+    if (is.null(within)) {
+      # If no grouping factor is specified,
+      # scale the data across the entire variable
+      data[,label] <- as.vector(
+        base::scale(data[,var])
+      )
+    } else {
+      # If there is a grouping factor specified,
+      # apply scale individually for each of its levels
+      for (i in unique(within_values)) {
+        data[within_values==i,label] <-
+          as.vector(
+            base::scale(
+              data[within_values==i, var],
+              center=center,
+              scale=scale
+            )
+          )
+      }
+    }
+  }
+
+  return(data)
+}
+
+
+#' Standardize mouse-tracking measures per level of other variables.
+#'
+#' Standardize selected mouse-tracking measures per level of one or more other
+#' variable, and store them in new variables. This function is a thin wrapper
+#' around \link{scale_within}, focussed on mouse-tracking data stored in a
+#' mousetrap data object.
+#'
+#' @param data a mousetrap data object created using one of the mt_import
+#'   functions (see \link{mt_example} for details).
+#' @param use a character string specifying which data should be used. By
+#'   default points to the \code{measures} data.frame created using
+#'   \link{mt_measures}.
+#' @param use_variables a vector specifying which variables should be
+#'   standardized. If unspecified, all variables will be standardized.
+#' @param within a character string specifying one or more variables in
+#'   \code{data[["data"]]}. All measures will be standardized separately for
+#'   each level of the variable (or for each combination of levels, if more than
+#'   one variable is specified). By default, points to the subject identifier
+#'   used in OpenSesame ("subject_nr").
+#' @param prefix a character string that is inserted before each standardized
+#'   variable. If an empty string is specified, the original variables are
+#'   replaced.
+#' @param center argument passed on to \link{scale}.
+#' @param scale argument passed on to \link{scale}.
+#'
+#' @return A mousetrap data object (see \link{mt_example})
+#' including the standardized measures.
+#'
+#' @seealso
+#' \link{scale_within} which is called by \code{mt_standardize}.
+#'
+#' \link{scale} for the R base scale function.
+#'
+#' @examples
+#' mt_example <- mt_measures(mt_example)
+#'
+#' # Standardize MAD and AD per subject
+#' mt_example <- mt_standardize(mt_example,
+#'   use_variables=c("MAD", "AD"),
+#'   within="subject_nr", prefix="z_")
+#'
+#' # Standardize MAD and AD per subject and Condition
+#' mt_example <- mt_standardize(mt_example,
+#'   use_variables=c("MAD", "AD"),
+#'   within=c("subject_nr", "Condition"),
+#'   prefix="z_")
+#'   
+#' @author
+#' Pascal J. Kieslich (\email{kieslich@@psychologie.uni-mannheim.de})
+#' 
+#' Felix Henninger
+#'   
+#' @export
+mt_standardize <-function(data, use="measures",
+  use_variables=NULL, within="subject_nr",
+  prefix="z_", center=TRUE, scale=TRUE) {
+
+  data[[use]] <- extract_data(data=data,use=use)
+  
+  # Extract within variable values
+  within <- data$data[rownames(data[[use]]), within, drop=FALSE]
+
+  # Combine within variable values to one
+  # vector coding each combination of values
+  # with a unique integer
+  within <- as.numeric(factor(
+    apply(within, 1, paste, collapse=";")
+  ))
+
+  # Apply the scale_within function
+  data[[use]] <- scale_within(
+    data[[use]],
+    variables=use_variables,
+    within=within,
+    prefix=prefix,
+    center=center,
+    scale=scale
+  )
+
+  return(data)
+}
+
+
+
+#' Standardize trajectories
+#' 
+#' \code{mt_scale_trajectories} standardizes any trajectory variable to
+#' a mean of 0 (if \code{code = TRUE}) and standard deviation 1 (if \code{
+#' scale = TRUE}).
+#' 
+#' The new trajectory is added to the object specified in \code{use}.
+#' 
+#' @inheritParams mt_time_normalize
+#' @param var_names character vector giving the labels of the to be 
+#'   standardized variables.
+#' @param transform function that takes a numeric matric as argument and
+#'   returns a numeric matric of same size with transformed values. If NULL
+#'   the original values are passed on to standardization.
+#' @param center logical specifying whether variables should be centered
+#'   (i.e., \code{mean = 0}). Can be logical vector, in which case the values 
+#'   of \code{scale} are mapped to the variables specified in \code{var_names}.
+#' @param scale logical or numeric specifying the scaling of the variables.
+#'   When logical, \code{scale = TRUE} normalizes the trajectory variable
+#'   to sd = 1, wheras \code{scale = FALSE} leaves the variable on its original
+#'   scale. When numeric, the trajectory variables are scaled by (.i.e., divided
+#'   by) the specific value in scale. Can also be numeric vector, in which case
+#'   the values of \code{scale} are mapped to the variables specified in
+#'   \code{var_names}.
+#' @param within logical specifying whether trajectory variables should be 
+#'   scaled within or across trajectories. I.e., scaling trajectories to mean = 0
+#'   and sd = 1 for \code{within == TRUE} means that every trajectory will have
+#'   mean = 0 and sd = 1 for with regard to the variables in \code{var_names}. 
+#'   \code{within == FALSE} on the other hand renders mean = 0 and sd = 1 only 
+#'   true in the aggregate that is when considering all trajectories. Can 
+#'   be logical vector, in which case the values of \code{scale} are mapped to 
+#'   the variables specified in \code{var_names}.
+#' @param prefix character string added to the names of the new 
+#'   standardized variables. If \code{prefix = ""} the original variables
+#'   will be overwritten.
+#' 
+#' @example
+#' # Normalize velocity
+#' mt_example = mt_derivatives(mt_example)
+#' mt_example = mt_scale_trajectories(mt_example,variables = 'vel')
+#' 
+#' @author  Dirk U. Wulff (\email{dirk.wulff@@gmail.com})
+#'  
+#' @return A mousetrap data object (see \link{mt_example}) with an additional 
+#'   variable in the object specified by \code{use} containing the standardized
+#'   trajectory variable. If a trajectory array was provided directly as \code{data}, 
+#'   only that array includig now the new variable will be returned.
+#'   
+#' @export 
+mt_scale_trajectories = function(data,
+                                 use = 'trajectories',
+                                 var_names,
+                                 transform = NULL, 
+                                 center = TRUE, 
+                                 scale = TRUE,
+                                 within = FALSE,
+                                 prefix="z_"){
+  
+
+  # extract trajectories
+  traj = extract_data(data = data, use = use)
+
+  # test if variables are in traj
+  if(!all(var_names %in% dimnames(traj)[[3]])) stop('Variables not found')
+
+  # store old names
+  nam = dimnames(traj)
+
+  # expand center
+  if(length(center) != length(var_names)){
+    if(length(center) == 1){
+      center = rep(center,length(var_names))
+    } else {
+      stop('center must be length 1 or match length of var_names')
+    }
+  }
+  
+  # expand scale
+  if(length(scale) != length(var_names)){
+    if(length(scale) == 1){
+      scale = rep(scale,length(var_names))
+    } else {
+      stop('scale must be length 1 or match length of var_names')
+    }
+  }
+  
+  # expand within
+  if(length(within) != length(var_names)){
+    if(length(within) == 1){
+      within = rep(within,length(var_names))
+    } else {
+      stop('within must be length 1 or match length of var_names')
+    }
+  }
+  
+  
+  # standardize all variables and add to traj
+  vars = list()
+  for(i in 1:length(var_names)){
+    
+    # extract trajectory variable
+    var = traj[,,var_names[i]]
+    
+    # transform trajectory variable
+    if(!is.null(transform)) var = do.call(transform,list(var))
+    
+    ### scale
+    # z standardizing within and across
+    if(is.logical(scale[i])){
+      
+      # within
+      if(within[i] == TRUE){
+        n_var = scale_rows(var, center = center[i], scale = scale[i])   
+      
+      # across  
+      } else {
+        n_var = scale_mat(var, center = center[i], scale = scale[i])
+      }
+    
+    # arbitrary standardizing
+    } else if(is.numeric(scale)){
+      
+      # within
+      if(within[i] == TRUE){
+        n_var = trans_rows(var,center = center[i], scale = scale[i])
+      
+      # across  
+      } else {
+        n_var = trans_mat(var,center = center[i], scale = scale[i])       
+      }
+    
+    # report inappropriare scale  
+    } else {
+      stop('scale must be logical or numeric')
+    } 
+
+    
+    # restore NAs
+    n_var[is.na(var)] = NA
+    vars[[i]] = n_var
+  }
+
+  # add new variables
+  names(vars) = paste0(prefix,var_names)
+  traj = mt_add_variables(traj,variables = vars)
+  
+  # store
+  if(class(data) == 'mousetrap'){ 
+    data[[use]] = traj
+    return(data)
+    } else {
+    return(traj)
+    }
+}
