f2d0fbe068eba0f72d4b30580fd18684dbf41e5d
Pascal J. Kieslich <pascal.kieslich@gmail.com>
2021-09-04 14:49:09 +0200

    Build package documentation

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // cleanAngles
 NumericMatrix cleanAngles(NumericMatrix as);
 RcppExport SEXP _mousetrap_cleanAngles(SEXP asSEXP) {
