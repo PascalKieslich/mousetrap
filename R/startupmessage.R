fcb5096b5fd4622daf5d61897a57d8d93ccab9b9
Pascal J. Kieslich <pascal.kieslich@gmail.com>
2024-01-06 23:28:32 +0100

    Increment version number

--- R/startupmessage.R
+++ R/startupmessage.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to mousetrap 3.2.2!")
+  packageStartupMessage("Welcome to mousetrap 3.2.3!")
   packageStartupMessage("Summary of recent changes: http://pascalkieslich.github.io/mousetrap/news/")
   packageStartupMessage("Forum for questions: https://forum.cogsci.nl/index.php?p=/categories/mousetrap")
 }
